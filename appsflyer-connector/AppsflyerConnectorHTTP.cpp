1b9000b44101e330dc9b4039f42514f3b4a7beeb
gilmor-af <117647282+gilmor-af@users.noreply.github.com>
2023-01-17 12:02:01 +0200

    naming fixes

---
+++ appsflyer-connector/AppsflyerConnectorHTTP.cpp
@@ -0,0 +1,183 @@
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+
+#include "steam/steam_api.h"
+#include "AppsflyerConnectorHTTP.h"
+#include "AppsflyerConnector.cpp"
+
+using namespace std;
+
+
+CAppsflyerConnectorHTTP* AppsflyerConnectorHTTP()
+{
+	static CAppsflyerConnectorHTTP inv;
+	return &inv;
+}
+
+CAppsflyerConnectorHTTP::CAppsflyerConnectorHTTP() {
+	SteamAPI_RunCallbacks();
+}
+
+void CAppsflyerConnectorHTTP::OnHTTPCallBack(HTTPRequestCompleted_t* pCallback, bool bIOFailure)
+{
+	if (bIOFailure) {
+		onCallbackFailure(pCallback);
+	}
+	else {
+		onCallbackSuccess(pCallback);
+
+		AppsflyerConnector afc(devkey, appID);
+
+		uint64 context = pCallback->m_ulContextValue;
+		switch (context)
+		{
+		case FIRST_OPEN_REQUEST:
+		case SESSION_REQUEST:
+			if (pCallback->m_eStatusCode == k_EHTTPStatusCode202Accepted)
+			{
+				afc.increase_AF_counter();
+			}
+			break;
+		case INAPP_EVENT_REQUEST:
+			break;
+		default:
+			break;
+		}
+	}
+	SteamHTTP()->ReleaseHTTPRequest(pCallback->m_hRequest);
+}
+
+void CAppsflyerConnectorHTTP::send_http_post(HTTPRequestHandle handle) {
+	SteamAPICall_t api_handle{};
+	bool res = SteamHTTP()->SendHTTPRequest(handle, &api_handle);
+	m_SteamAPICallCompleted.Set(api_handle, this, &CAppsflyerConnectorHTTP::OnHTTPCallBack);
+	SteamAPI_RunCallbacks();
+}
+
+void CAppsflyerConnectorHTTP::onCallbackSuccess(HTTPRequestCompleted_t* pCallback) {
+	// Handle Success
+	uint64 context = pCallback->m_ulContextValue;
+	switch (context)
+	{
+		case FIRST_OPEN_REQUEST:
+		case SESSION_REQUEST:
+			// ** handle success for these callback **
+			break;
+		case INAPP_EVENT_REQUEST:
+			// ** handle success for this callback **
+			break;		
+		default:
+			break;
+	}
+}
+
+void CAppsflyerConnectorHTTP::onCallbackFailure(HTTPRequestCompleted_t* pCallback) {
+	// Handle Failure
+	uint64 context = pCallback->m_ulContextValue;
+	switch (context)
+	{
+		case FIRST_OPEN_REQUEST:
+		case SESSION_REQUEST:
+			// ** handle failure for these callback **
+			break;
+		case INAPP_EVENT_REQUEST:
+			// ** handle failure for this callback **
+			break;
+		default:
+			break;
+	}
+}
+
+void CAppsflyerConnectorHTTP::start(const char * dkey, const char * appid) {
+	// testing af_firstOpen/af_session and af_inappEvent 
+	devkey = dkey;
+	appID = appid;
+	AppsflyerConnector afc(devkey, appID);
+	CSteamID usrID = SteamUser()->GetSteamID();
+	const auto steamIDInt = SteamUser()->GetSteamID().ConvertToUint64();
+	std::ostringstream os;
+	os << steamIDInt;
+	std::string steamID = os.str();
+
+	//steam app build id
+	int bid = SteamApps()->GetAppBuildId();
+	std::string app_version = std::to_string(bid);
+
+	//create timestamp
+	std::time_t t = std::time(0);
+	std::ostringstream oss;
+	oss << t;
+	std::string timestamp = oss.str();
+
+	RequestData req;
+	req.timestamp = timestamp;
+	req.device_os_version = "1.0.0";
+	req.app_version = app_version;
+	req.device_model = afc.get_OS(); //TODO: check how to retreive device model - in the meantime send 'steam'
+	req.limit_ad_tracking = "false";
+	req.request_id = afc.uuid_gen().c_str();
+
+	//adding AF id to the request
+	DeviceIDs af_id;
+	af_id.type = "custom";
+	af_id.value = afc.get_AF_id().c_str();
+	req.device_ids.insert(req.device_ids.end(), af_id);
+
+	//adding steam uid to the request - TODO: add to request json after approved by the server
+	DeviceIDs steam_id;
+	steam_id.type = "custom";
+	steam_id.value = steamID.c_str();
+	req.device_ids.insert(req.device_ids.end(), steam_id);
+
+	SteamAPICall_t api_handle{};
+	HTTPRequestHandle reqH = afc.af_firstOpen_init(req);
+	bool res = SteamHTTP()->SendHTTPRequest(reqH, &api_handle);
+	m_SteamAPICallCompleted.Set(api_handle, this, &CAppsflyerConnectorHTTP::OnHTTPCallBack);
+	SteamAPI_RunCallbacks();
+
+	//AppsflyerConnectorHTTP()->send_http_post(afc.af_firstOpen_init(req));
+}
+
+void CAppsflyerConnectorHTTP::logEvent(std::string event_name, json event_values) {
+	AppsflyerConnector afc("sQ84wpdxRTR4RMCaE9YqS4", "480");
+	CSteamID usrID = SteamUser()->GetSteamID();
+	const auto steamIDInt = SteamUser()->GetSteamID().ConvertToUint64();
+	std::ostringstream os;
+	os << steamIDInt;
+	std::string steamID = os.str();
+
+	//steam app build id
+	int bid = SteamApps()->GetAppBuildId();
+	std::string app_version = std::to_string(bid);
+
+	//create timestamp
+	std::time_t t = std::time(0);
+	std::ostringstream oss;
+	oss << t;
+	std::string timestamp = oss.str();
+
+	RequestData req;
+	req.timestamp = timestamp;
+	req.device_os_version = "1.0.0";
+	req.app_version = app_version;
+	req.device_model = afc.get_OS(); //TODO: check how to retreive device model - in the meantime send 'steam'
+	req.limit_ad_tracking = "false";
+	req.request_id = afc.uuid_gen().c_str();
+
+	//adding AF id to the request
+	DeviceIDs af_id;
+	af_id.type = "custom";
+	af_id.value = afc.get_AF_id().c_str();
+	req.device_ids.insert(req.device_ids.end(), af_id);
+
+	//adding steam uid to the request - TODO: add to request json after approved by the server
+	DeviceIDs steam_id;
+	steam_id.type = "custom";
+	steam_id.value = steamID.c_str();
+	req.device_ids.insert(req.device_ids.end(), steam_id);
+
+	req.event_name = event_name;
+	req.event_values = event_values;
+
+	AppsflyerConnectorHTTP()->send_http_post(afc.af_inappEvent(req));
+}
